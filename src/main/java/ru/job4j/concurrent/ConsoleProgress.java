72543400e70bfa3449514f8cee9205bbc4a1a809
fromiva <66008363+fromiva@users.noreply.github.com>
2023-05-06 07:55:29 +0300

    5. Прерывание нити [#1019]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,11 +1,11 @@
 package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
-    char[] process = new char[]{'-', '\\', '|', '/'};
 
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
+            char[] process = new char[]{'-', '\\', '|', '/'};
             try {
                 for (char c : process) {
                     System.out.print("\r load: " + c);
