bff98cd91a8543e16865cf3e1e59e54d0bf8e1af
fromiva <66008363+fromiva@users.noreply.github.com>
2023-05-08 11:53:39 +0300

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/ru/job4j/atomicity/Cache.java
@@ -0,0 +1,12 @@
+package ru.job4j.atomicity;
+
+public class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache getInstance() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
