8410af029ac752741122aff60fd4fd5e2349c842
fromiva <66008363+fromiva@users.noreply.github.com>
2023-05-12 14:39:05 +0300

    3. Денежные переводы AccountStorage [#1104]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -4,53 +4,40 @@ import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.HashMap;
-import java.util.Objects;
 import java.util.Optional;
 
 @ThreadSafe
 public class AccountStorage {
-    @GuardedBy("accounts")
+    @GuardedBy("this")
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
-    public boolean add(Account account) {
-        Objects.requireNonNull(account);
-        synchronized (accounts) {
-            return accounts.putIfAbsent(account.id(), account) == null;
-        }
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
     }
 
-    public boolean update(Account account) {
-        Objects.requireNonNull(account);
-        synchronized (accounts) {
-            return accounts.replace(account.id(), account) != null;
-        }
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
     }
 
-    public boolean delete(int id) {
-        synchronized (accounts) {
-            return accounts.remove(id) != null;
-        }
+    public synchronized boolean delete(int id) {
+        return accounts.remove(id) != null;
     }
 
-    public Optional<Account> getById(int id) {
-        synchronized (accounts) {
-            return Optional.ofNullable(accounts.get(id));
-        }
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
     }
 
-    public boolean transfer(int fromId, int toId, int amount) {
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
         if (amount < 0) {
             throw new IllegalArgumentException("Negative amount is not allowed.");
         }
-        synchronized (accounts) {
-            Optional<Account> from = getById(fromId);
-            Optional<Account> to = getById(toId);
-            boolean result = from.isPresent() && to.isPresent() && from.get().amount() >= amount;
-            if (result) {
-                update(new Account(from.get().id(), from.get().amount() - amount));
-                update(new Account(to.get().id(), to.get().amount() + amount));
-            }
-            return result;
+        Optional<Account> from = getById(fromId);
+        Optional<Account> to = getById(toId);
+        boolean result = from.isPresent() && to.isPresent() && from.get().amount() >= amount;
+        if (result) {
+            update(new Account(from.get().id(), from.get().amount() - amount));
+            update(new Account(to.get().id(), to.get().amount() + amount));
         }
+        return result;
     }
 }
