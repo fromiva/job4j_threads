182bc06f2cf3ade145208287da702b09039fb6b0
fromiva <66008363+fromiva@users.noreply.github.com>
2023-05-17 10:58:43 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -0,0 +1,22 @@
+package ru.job4j.email;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors
+            .newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(() -> send(
+                String.format("Notification %s to email %s", user.username(), user.email()),
+                String.format("Add a new event to %s", user.username()),
+                user.email()));
+    }
+
+    public void send(String subject, String body, String email) { }
+
+    public void close() {
+        pool.shutdown();
+    }
+}
---
+++ src/main/java/ru/job4j/email/User.java
@@ -0,0 +1,3 @@
+package ru.job4j.email;
+
+public record User(String username, String email) { }
